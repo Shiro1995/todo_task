42fadab205ce09717420ad80dfde3f2202ace1de
Shiro1995 <tnt.giuma@gmail.com>
2022-01-15 08:30:14 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Text('Hello World'),
+    );
+  }
+}
